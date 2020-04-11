2469685b14d4d8477a26c738b3ced10a5e515376
cessareoo <cesarwm22@gmail.com>
2020-04-11 23:16:25 +0200

    FunctionalTests Get verb

---
+++ tests/FunctionalTests/Web/Controllers/NotesControllerGet.cs
@@ -0,0 +1,42 @@
+﻿using ApplicationCore.Entities;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FunctionalTests.Web.Controllers
+{
+    [Collection("Sequential")]
+    public class NotesControllerGet : IClassFixture<WebTestFixture>
+    {
+        public NotesControllerGet(WebTestFixture factory)
+        {
+            Client = factory.CreateClient();
+        }
+
+        public HttpClient Client { get; }
+
+        [Fact]
+        public async Task ReturnsTwoNotes()
+        {
+            var response = await Client.GetAsync("/api/notes");
+            response.EnsureSuccessStatusCode();
+            var stringResponse = await response.Content.ReadAsStringAsync();
+            var model = JsonConvert.DeserializeObject<List<Note>>(stringResponse);
+
+            Assert.Equal(2, model.Count);
+        }
+
+        [Fact]
+        public async Task ReturnsNote()
+        {
+            var response = await Client.GetAsync("/api/notes/1");
+            response.EnsureSuccessStatusCode();
+            var stringResponse = await response.Content.ReadAsStringAsync();
+            var model = JsonConvert.DeserializeObject<Note>(stringResponse);
+
+            Assert.NotNull(model);
+        }
+    }
+}
---
+++ tests/FunctionalTests/Web/WebTestFixture.cs
@@ -0,0 +1,61 @@
+﻿using Infraestructure.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using Web;
+
+namespace FunctionalTests.Web
+{
+    public class WebTestFixture: WebApplicationFactory<Startup>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.UseEnvironment("Testing");
+
+            builder.ConfigureServices(services =>
+            {
+                services.AddEntityFrameworkInMemoryDatabase();
+
+                //Create a new service provider.
+                var provider = services
+                                .AddEntityFrameworkInMemoryDatabase()
+                                .BuildServiceProvider();
+
+                //Add a database context (NotePadContext) using an in-memory
+                services.AddDbContext<NotePadContext>(options =>
+                {
+                    options.UseInMemoryDatabase("InMemoryDbForTesting");
+                    options.UseInternalServiceProvider(provider);
+                });
+
+                //Build the service provider
+                var sp = services.BuildServiceProvider();
+
+                //Create a scope to obtain a reference to the database
+                using(var scope = sp.CreateScope())
+                {
+                    var scoppedServices = scope.ServiceProvider;
+                    var db = scoppedServices.GetRequiredService<NotePadContext>();
+
+                    var logger = scoppedServices.GetRequiredService<ILogger<WebTestFixture>>();
+
+                    //Ensure the database is created
+                    db.Database.EnsureCreated();
+                    try
+                    {
+                        NotePadContextSeed.Initialize(db);
+                    }
+                    catch(Exception ex)
+                    {
+                        logger.LogError(ex, $"An error occurred seeding the " +
+                            $"database with test messages. Error: {ex.Message}");
+                    }
+                }
+            });
+            base.ConfigureWebHost(builder); 
+        }
+    }
+}

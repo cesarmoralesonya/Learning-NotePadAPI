61d272a8d4ea9ec781e89647a35492a59e2b3463
cessareoo <cesarwm22@gmail.com>
2020-04-16 18:30:16 +0200

    Delete code is not used

--- src/ApplicationCore/Interfaces/IAsyncRepository.cs
+++ src/ApplicationCore/Interfaces/IAsyncRepository.cs
@@ -8,10 +8,8 @@ namespace ApplicationCore.Interfaces
     {
         Task<T> GetByIdAsync(int id);
         Task<IReadOnlyList<T>> ListAllAsync();
-        Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec);
         Task<T> AddAsync(T entity);
         Task UpdateAsync(T entity);
         Task DeleteAsync(T entity);
-        Task<int> CountAsync(ISpecification<T> spec);
     }
 }
--- src/Infraestructure/Data/Repositories/EfRepository.cs
+++ src/Infraestructure/Data/Repositories/EfRepository.cs
@@ -2,7 +2,6 @@
 using ApplicationCore.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Infraestructure.Data.Repositories
@@ -46,21 +45,6 @@ namespace Infraestructure.Data.Repositories
         public async Task<IReadOnlyList<T>> ListAllAsync()
         {
             return await _dbContext.Set<T>().ToListAsync();
-        }
-
-        public async Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec)
-        {
-            return await ApplySpecification(spec).ToListAsync();
-        }
-
-        public async Task<int> CountAsync(ISpecification<T> spec)
-        {
-            return await ApplySpecification(spec).CountAsync();
-        }
-
-        private IQueryable<T> ApplySpecification(ISpecification<T> spec)
-        {
-            return SpecificationEvaluator<T>.GetQuery(_dbContext.Set<T>().AsQueryable(), spec);
         }
     }
 }
